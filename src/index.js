636f56d148cd2f41cbdd8e6836461c62672ee4e9
Jascha <jascha@jaeh.at>
2018-12-12 18:56:07 +0100

    css itself returns an object with 4 props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const is = require('@magic/types')
-const fs = require('fs')
-const path = require('path')
 
-const css = {
-  parse: require('./parse'),
-  stringify: require('./stringify'),
-  write: require('./write'),
-  minify: require('./minify'),
-  classes: require('./classes'),
-}
+const parse = require('./parse')
+const stringify = require('./stringify')
+const write = require('./write')
+const minify = require('./minify')
+const classes = require('./classes')
+
+const css = style => !is.empty(style) && ({
+  css: stringify(style),
+  classes: classes(style),
+  parsed: parse(style),
+  minified: minify(stringify(style)),
+})
+
+css.parse = parse
+css.stringify = stringify
+css.write = write
+css.minify = minify
+css.classes = classes
 
 module.exports = css
