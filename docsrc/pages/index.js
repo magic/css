3a7c4e31c80064dd8d9002d4d99ae86de9a9a86c
Jascha <jascha@jaeh.at>
2019-04-20 16:04:41 +0200

    use Badges module

--- docsrc/pages/index.js
+++ docsrc/pages/index.js
@@ -4,12 +4,7 @@ module.exports = () => div([
   p('parse/stringify/write css in js'),
   p('NO dynamic css, css gets output as css file'),
 
-  Link({ to: 'https://www.npmjs.com/package/@magic/css', }, Img({ src: "https://img.shields.io/npm/v/@magic/css.svg" })),
-  Link({ to: 'https://travis-ci.org/magic/css' }, Img({ src: 'https://travis-ci.com/magic/css.svg?branch=master' })),
-  Link({ to: 'https://ci.appveyor.com/project/jaeh/css/branch/master' }, Img({ src: 'https://ci.appveyor.com/api/projects/status/yk1hmw7ilwb74h5y/branch/master?svg=true' })),
-  Link({ to: 'https://coveralls.io/github/magic/css' }, Img({ src: 'https://coveralls.io/repos/github/magic/css/badge.svg' })),
-  Link({ to: 'https://greenkeeper.io' }, Img({ src: 'https://badges.greenkeeper.io/magic/css.svg' })),
-
+  Badges,
 
   h2('installation'),
   Pre.View('npm install @magic/css'),
