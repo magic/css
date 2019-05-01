beb846ad6df71054082bcad9d34945811ef838d4
Jascha <jascha@jaeh.at>
2019-05-02 00:29:45 +0200

    write now only throws and does not log

--- src/write.js
+++ src/write.js
@@ -3,8 +3,7 @@ const stringify = require('./stringify')
 
 const write = (style, opts = {}) => {
   if (!opts.OUTFILE) {
-    console.error(opts)
-    throw new Error('Can not write css without opts.OUTFILE')
+    throw new Error(`Can not write css without opts.OUTFILE ${JSON.stringify(opts.OUTFILE, null, 2)}`)
   }
 
   const bundle = stringify(style, opts)
