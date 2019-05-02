33eb307844c30253f4079dcf8c0881fa0021ee18
Jascha <jascha@jaeh.at>
2019-05-02 02:15:23 +0200

    format

--- src/write.js
+++ src/write.js
@@ -3,7 +3,9 @@ const stringify = require('./stringify')
 
 const write = (style, opts = {}) => {
   if (!opts.OUTFILE) {
-    throw new Error(`Can not write css without opts.OUTFILE ${JSON.stringify(opts.OUTFILE, null, 2)}`)
+    throw new Error(
+      `Can not write css without opts.OUTFILE ${JSON.stringify(opts.OUTFILE, null, 2)}`,
+    )
   }
 
   const bundle = stringify(style, opts)
