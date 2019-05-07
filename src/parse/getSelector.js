b3772ec01e80b80b1c8b78194798007dedae8a93
Jascha <jascha@jaeh.at>
2019-05-07 04:24:19 +0200

    be fully recursive, accept & at start and end of selectorgs

--- src/parse/getSelector.js
+++ src/parse/getSelector.js
@@ -1,24 +1,50 @@
 const getSelector = (parent, name) => {
-  // names that contain , have to be split and prefixed separately
-  if (name.includes(',')) {
-    return name
+  name = name.trim()
+
+  // if parents are an array, loop over them
+  if (parent.includes(',')) {
+    return parent
       .split(',')
-      .map(n => getSelector(parent, n.trim()))
+      .map(par => getSelector(par.trim(), name))
       .join(', ')
       .trim()
   }
 
-  // compose the selector
-  let res = parent
-
-  // selectors starting with & get appended to parent
+  // find out if we have to prefix, suffix, or assume that we want to designate a child class
+  let before = ''
+  let after = ''
   if (name.startsWith('&')) {
-    res += name.substr(1)
-  } else {
-    res += ` ${name}`
+    before = '&'
+  } else if (name.endsWith('&')) {
+    after = '&'
   }
 
-  return res.trim()
+  // names that contain , have to be split and prefixed separately
+  if (name.includes(',')) {
+    return name
+      .split(',')
+      .map(n => `${before}${n}${after}`)
+      .map(n => getSelector(parent, n))
+      .join(', ')
+      .trim()
+  }
+
+  // make sure our name does not contain any &'s after this line
+  name = name.replace(/&/g, '').trim()
+
+  // make sure our parent does not contain any &'s after this line
+  parent = parent.replace(/&/g, '').trim()
+
+  // merge selector depending on the before and after variables defined above
+  if (before) {
+    parent += name
+  } else if (after) {
+    parent = name + parent
+  } else {
+    parent += ` ${name}`
+  }
+
+  return parent.trim()
 }
 
 module.exports = getSelector
