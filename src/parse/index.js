9ffacfd08a4948f96bebf12c008c3db85e650346
Jascha <jascha@jaeh.at>
2019-05-10 19:09:31 +0200

    refactor parsing, now returns nicer ordered css

--- src/parse/index.js
+++ src/parse/index.js
@@ -1,11 +1,24 @@
 const is = require('@magic/types')
 const deep = require('@magic/deep')
+const log = require('@magic/log')
 
 const getSelector = require('./getSelector')
-const recurseStyle = require('./style')
 
 const recurseParse = mod => {
+  if (!is.array(mod) && is.object(mod)) {
+    return Object.entries(mod).map(recurseParse)
+  }
+
   const [parent, items] = mod
+
+  if (is.object(parent)) {
+    return Object.entries(parent).map(recurseParse)
+  }
+
+  if (!is.object(items)) {
+    log.error('Invalid style received', [parent, items])
+    return
+  }
 
   let children = []
   const props = {}
@@ -38,21 +51,51 @@ const recurseParse = mod => {
   }
 }
 
+const isStyle = style => style && style.length === 2 && is.string(style[0]) && is.object(style[1])
+
+const concatMaps = (map, ...iterables) => {
+  for (const iterable of iterables) {
+    for (const [key, val] of iterable) {
+      map.set(key, val)
+    }
+  }
+  return map
+}
+
+const flatten = styles => {
+  let styleMap = new Map()
+
+  if (isStyle(styles)) {
+    styleMap.set(...styles)
+  } else if (is.array(styles)) {
+    styles
+      .filter(a => a)
+      .forEach(style => {
+        styleMap = concatMaps(styleMap, flatten(style))
+      })
+  } else {
+    return styles
+  }
+
+  return styleMap
+}
+
 const parse = (styles, opts = {}) => {
   // first check if the user sent us a function that resolves to a css object
   if (is.function(styles)) {
     styles = styles(opts)
   }
+
   // this might trigger additionally to the is.function if statement above
   if (is.array(styles)) {
-    styles = recurseStyle(styles)
+    styles = styles.map(recurseParse)
+  } else if (!is.array(styles) && is.object(styles)) {
+    styles = Object.entries(styles).map(recurseParse)
   }
 
-  const parsed = Object.entries(styles)
-    .sort(a => (a[0].startsWith('@') ? 1 : -1))
-    .map(recurseParse)
+  const parsedMap = flatten(styles)
 
-  return parsed
+  return Array.from(parsedMap)
 }
 
 module.exports = parse
