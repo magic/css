61172b5493fbea3e31f851c5dfc75370d3c4ddf1
Jascha <jascha@jaeh.at>
2019-12-28 17:50:07 +0100

    manually hoist gdpr

--- config.mjs
+++ config.mjs
@@ -4,12 +4,5 @@ export default {
   THEME: 'docs',
   URL: 'magic.github.io/css',
   PUBLIC: 'docs',
-
-  // this option adds the
-  // 'X-Clacks-Overhead', 'GNU Terry Pratchet'
-  // http header
-  // see http://www.gnuterrypratchett.com/
-  FOR_DEATH_CAN_NOT_HAVE_HIM: true,
-
-  KEEP_CLIENT: false,
+  HOIST: 'Gdpr',
 }
