ef6010c818093e2252be77d871a5c09ec523f063
jaeh <jascha@jaeh.at>
2021-07-11 02:12:09 +0200

    update tests to use @magic/cases instead of local implementation

--- test/camel2kebab.mjs
+++ test/camel2kebab.mjs
@@ -1,5 +1,8 @@
-import camel2kebab from '../src/camel2kebab.mjs'
+import cases from '@magic/cases'
 
 export default [
-  { fn: camel2kebab('backgroundColor'), expect: 'background-color', info: 'can camel 2 kebab' },
+  { fn: cases.kebab('backgroundColor'), expect: 'background-color', info: 'can camel 2 kebab' },
+  { fn: cases.kebab('background_Color'), expect: 'background-color', info: 'can camel 2 kebab' },
+  { fn: cases.kebab('background_color'), expect: 'background-color', info: 'can camel 2 kebab' },
+  { fn: cases.kebab('BACKGROUND_COLOR'), expect: 'background-color', info: 'can camel 2 kebab' },
 ]
