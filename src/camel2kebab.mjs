2fc2cb58ce225daabf8e95c0e069625dd75a24a9
jaeh <jascha@jaeh.at>
2021-07-11 02:12:19 +0200

    remove src/camel2kebab

--- src/camel2kebab.mjs
+++
@@ -1,6 +0,0 @@
-const camReg = /[a-z]([A-Z])+/g
-const camHandler = m => m[0] + '-' + m.substring(1).toLowerCase()
-// converts js camelCase css Objects (backgroundColor) to css kebab (background-color)
-const camel2kebab = str => str.replace(camReg, camHandler)
-
-export default camel2kebab
