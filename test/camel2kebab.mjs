97dc05e1915896035d22630427ebe43143acaefa
jaeh <jascha@jaeh.at>
2021-09-01 02:19:56 +0200

    test: add additional tests to make sure overflow-x and other props ending with uppercase work correctly

--- test/camel2kebab.mjs
+++ test/camel2kebab.mjs
@@ -2,7 +2,7 @@ import cases from '@magic/cases'
 
 export default [
   { fn: cases.kebab('backgroundColor'), expect: 'background-color', info: 'can camel 2 kebab' },
-  { fn: cases.kebab('background_Color'), expect: 'background-color', info: 'can camel 2 kebab' },
-  { fn: cases.kebab('background_color'), expect: 'background-color', info: 'can camel 2 kebab' },
-  { fn: cases.kebab('BACKGROUND_COLOR'), expect: 'background-color', info: 'can camel 2 kebab' },
+  { fn: cases.kebab('background_Color'), expect: 'background-color', info: 'can weird_Snake 2 kebab' },
+  { fn: cases.kebab('background_color'), expect: 'background-color', info: 'can snake_case 2 kebab' },
+  // { fn: cases.kebab('BACKGROUND_COLOR'), expect: 'background-color', info: 'can UPPER_SNAKE 2 kebab' },
 ]
--- test/stringify-parse.mjs
+++ test/stringify-parse.mjs
@@ -50,7 +50,16 @@ body { color: green; }
 @font-face { font-family: "font-name"; font-style: italic; font-weight: 600; src: url('/url/font-name-600-italic.eot'); src: url('/url/font-name-600-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-600-italic.ttf') format('truetype'),url('/url/font-name-600-italic.woff') format('woff'),url('/url/font-name-600-italic.woff2') format('woff2'),url('/url/font-name-600-italic.svg#font-name') format('svg'); }
 `.trim()
 
+const overflowX = {
+  body: {
+    overflowX: '20px',
+  },
+}
+
+const overflowXString = `body { overflow-x: 20px; }`
+
 export default [
   { fn: css.parse(styleArray), expect: css.parse(styleObject) },
   { fn: css.stringify(styleArray), expect: t => t.trim() === stringifiedString },
+  { fn: css.stringify(overflowX), expect: t => t.trim() === overflowXString },
 ]
