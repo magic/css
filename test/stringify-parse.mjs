45233dbae4084cb4fafd0df7f6d91b57a6c88b33
jascha <j@jaeh.at>
2021-12-19 17:25:01 +0100

    test/stringify-parse: change font order to write woff2 before woff

--- test/stringify-parse.mjs
+++ test/stringify-parse.mjs
@@ -7,7 +7,7 @@ const styleObject = {
   '@font-face': {
     url: '/url/',
     family: 'font-name',
-    types: ['eot', 'ttf', 'woff', 'woff2', 'svg'],
+    types: ['eot', 'ttf', 'woff2', 'woff', 'svg'],
     weights: [400, 500, 600],
     styles: ['normal', 'italic'],
   },
@@ -27,7 +27,7 @@ const styleArray = [
       '@font-face': {
         url: '/url/',
         family: 'font-name',
-        types: ['eot', 'ttf', 'woff', 'woff2', 'svg'],
+        types: ['eot', 'ttf', 'woff2', 'woff', 'svg'],
         weights: [400, 500, 600],
         styles: ['normal', 'italic'],
       },
@@ -37,18 +37,18 @@ const styleArray = [
 
 const stringifiedString = `
 body { color: green; }
- @font-face { font-family: "font-name"; font-style: normal; font-weight: 400; src: url('/url/font-name-400-normal.eot'); src: url('/url/font-name-400-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-400-normal.ttf') format('truetype'),url('/url/font-name-400-normal.woff') format('woff'),url('/url/font-name-400-normal.woff2') format('woff2'),url('/url/font-name-400-normal.svg#font-name') format('svg'); }
+ @font-face { font-family: "font-name"; font-style: normal; font-weight: 400; src: url('/url/font-name-400-normal.eot'); src: url('/url/font-name-400-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-400-normal.woff2') format('woff2'),url('/url/font-name-400-normal.woff') format('woff'),url('/url/font-name-400-normal.ttf') format('truetype'),url('/url/font-name-400-normal.svg#font-name') format('svg'); }
 
-@font-face { font-family: "font-name"; font-style: italic; font-weight: 400; src: url('/url/font-name-400-italic.eot'); src: url('/url/font-name-400-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-400-italic.ttf') format('truetype'),url('/url/font-name-400-italic.woff') format('woff'),url('/url/font-name-400-italic.woff2') format('woff2'),url('/url/font-name-400-italic.svg#font-name') format('svg'); }
+@font-face { font-family: "font-name"; font-style: italic; font-weight: 400; src: url('/url/font-name-400-italic.eot'); src: url('/url/font-name-400-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-400-italic.woff2') format('woff2'),url('/url/font-name-400-italic.woff') format('woff'),url('/url/font-name-400-italic.ttf') format('truetype'),url('/url/font-name-400-italic.svg#font-name') format('svg'); }
 
-@font-face { font-family: "font-name"; font-style: normal; font-weight: 500; src: url('/url/font-name-500-normal.eot'); src: url('/url/font-name-500-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-500-normal.ttf') format('truetype'),url('/url/font-name-500-normal.woff') format('woff'),url('/url/font-name-500-normal.woff2') format('woff2'),url('/url/font-name-500-normal.svg#font-name') format('svg'); }
+@font-face { font-family: "font-name"; font-style: normal; font-weight: 500; src: url('/url/font-name-500-normal.eot'); src: url('/url/font-name-500-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-500-normal.woff2') format('woff2'),url('/url/font-name-500-normal.woff') format('woff'),url('/url/font-name-500-normal.ttf') format('truetype'),url('/url/font-name-500-normal.svg#font-name') format('svg'); }
 
-@font-face { font-family: "font-name"; font-style: italic; font-weight: 500; src: url('/url/font-name-500-italic.eot'); src: url('/url/font-name-500-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-500-italic.ttf') format('truetype'),url('/url/font-name-500-italic.woff') format('woff'),url('/url/font-name-500-italic.woff2') format('woff2'),url('/url/font-name-500-italic.svg#font-name') format('svg'); }
+@font-face { font-family: "font-name"; font-style: italic; font-weight: 500; src: url('/url/font-name-500-italic.eot'); src: url('/url/font-name-500-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-500-italic.woff2') format('woff2'),url('/url/font-name-500-italic.woff') format('woff'),url('/url/font-name-500-italic.ttf') format('truetype'),url('/url/font-name-500-italic.svg#font-name') format('svg'); }
 
-@font-face { font-family: "font-name"; font-style: normal; font-weight: 600; src: url('/url/font-name-600-normal.eot'); src: url('/url/font-name-600-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-600-normal.ttf') format('truetype'),url('/url/font-name-600-normal.woff') format('woff'),url('/url/font-name-600-normal.woff2') format('woff2'),url('/url/font-name-600-normal.svg#font-name') format('svg'); }
+@font-face { font-family: "font-name"; font-style: normal; font-weight: 600; src: url('/url/font-name-600-normal.eot'); src: url('/url/font-name-600-normal.eot#iefix') format('embedded-opentype'),url('/url/font-name-600-normal.woff2') format('woff2'),url('/url/font-name-600-normal.woff') format('woff'),url('/url/font-name-600-normal.ttf') format('truetype'),url('/url/font-name-600-normal.svg#font-name') format('svg'); }
 
-@font-face { font-family: "font-name"; font-style: italic; font-weight: 600; src: url('/url/font-name-600-italic.eot'); src: url('/url/font-name-600-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-600-italic.ttf') format('truetype'),url('/url/font-name-600-italic.woff') format('woff'),url('/url/font-name-600-italic.woff2') format('woff2'),url('/url/font-name-600-italic.svg#font-name') format('svg'); }
-`.trim()
+@font-face { font-family: "font-name"; font-style: italic; font-weight: 600; src: url('/url/font-name-600-italic.eot'); src: url('/url/font-name-600-italic.eot#iefix') format('embedded-opentype'),url('/url/font-name-600-italic.woff2') format('woff2'),url('/url/font-name-600-italic.woff') format('woff'),url('/url/font-name-600-italic.ttf') format('truetype'),url('/url/font-name-600-italic.svg#font-name') format('svg'); }
+`.trimStart()
 
 const overflowX = {
   body: {
