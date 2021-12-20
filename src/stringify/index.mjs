498823e9eb235957b6992711ce26a002eee24b8f
jascha <j@jaeh.at>
2021-12-20 23:24:11 +0100

    stringify|parse: add minimal comments

--- src/stringify/index.mjs
+++ src/stringify/index.mjs
@@ -4,11 +4,19 @@ import autoprefixer from 'autoprefixer'
 import parse from '../parse/index.mjs'
 import recurseStringify from './recurseStringify.mjs'
 
+/*
+ * stringify the styles css object into a valid css string.
+ */
 const stringify = async (styles, opts = {}) => {
   const parsed = parse(styles, opts)
 
   const stringified = recurseStringify(parsed)
-  const result = await postcss([autoprefixer]).process(stringified, { from: undefined })
+
+  /*
+   * test the resulting css using postcss and autoprefix vendors where needed.
+   */
+  const result = await postcss([ autoprefixer ]).process(stringified, { from: undefined })
+
   result.warnings().forEach(warn => {
     console.warn(warn.toString())
   })
