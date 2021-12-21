96b0db6c82262d19c6f5e1e791721116f17583a2
jascha <j@jaeh.at>
2021-12-21 22:50:14 +0100

    add test/overload to test css overloads

---
+++ test/overload.mjs
@@ -0,0 +1,17 @@
+import css from '../src/index.mjs'
+
+const overload = {
+  body: {
+    color: [ 'green', 'red' ],
+  },
+}
+
+const overloadString = `body { color: green; color: red; }\n`
+
+export default [
+  {
+    fn: css.stringify(overload, { debug: true }),
+    expect: overloadString,
+    info: 'overloading css properties works',
+  },
+]
