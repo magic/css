1b2d507632e911cb8b39e150c8db3e1fc62c2450
jascha <j@jaeh.at>
2021-12-21 03:57:54 +0100

    bin/index: add commands, more help, parse, stringify and full work

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,53 +1,101 @@
 #!/usr/bin/env node
 
+import path from 'path'
+
 import cli from '@magic/cli'
 import fs from '@magic/fs'
 import log from '@magic/log'
 
 import css from '../index.mjs'
 
-// import format from './index.mjs'
+const cwd = process.cwd()
 
-const { args } = cli({
+const { args, commands } = cli({
+  commands: ['full', 'parse', 'stringify'],
   options: [
-    ['--parse', '--p', '-p'],
-    ['--stringify', '--s', '-s'],
-    ['--source', '--src'],
-    ['--dest', '--d', '-d'],
+    ['--in', '--i', '-i'],
+    ['--out', '--o', '-o'],
+    ['--minified', '--m', '-m'],
   ],
-  single: ['--parse', '--stringify'],
+  single: ['--in', '--out', '--minified'],
   help: {
     name: '@magic/css',
     header: 'css in js pipeline.',
+    commands: {
+      full: 'get a full result object.',
+      parse: 'convert css in js to an array of key value pairs',
+      stringify: 'convert css in js to css',
+    },
     options: {
-      '--parse': 'parse css and convert to js',
-      '--file-types': 'parse js and convert to css',
-      '--source': 'directory with source files',
+      '--in': 'directory with source files, needs index.js to exist',
       '--out': 'directory to write output files to. omit to print to stdout',
+      '--minified': 'output minified css',
     },
     example: `
-css --parse --dir ./css --out ./css
-css --stringify --dir ./styles --out ./css
+mcss parse --dir ./css --out ./css
+mcss stringify --dir ./styles --out ./css
 `.trim(),
   },
 })
 
+const logResult = style => log.success('mcss output', `\n\n${style}\n`)
+
+const maybeWrite = async ({ args, commands, style }) => {
+  const outFile = commands.stringify ? 'main.css' : 'theme.js'
+
+  if (args.out) {
+    if (!path.isAbsolute(args.out)) {
+      args.out = path.join(cwd, args.out)
+    }
+
+    if (!args.out.endsWith('.css') && !args.out.endsWith('.js')) {
+      args.out = path.join(args.out, outFile)
+    }
+
+    const dirname = path.dirname(args.out)
+    await fs.mkdirp(dirname)
+
+    await fs.writeFile(args.out, style)
+  } else {
+    logResult(style)
+  }
+
+  log.success('mcss is done.')
+}
+
 const run = async () => {
-  console.log({ args })
-
-  if (args.parse === false && args.stringify === false) {
-    log.error('MISSING_ARGUMENT', 'Either --parse or --stringify are required.')
-    process.exit(1)
+  if (commands.all) {
+    log.error('E_NOT_SUPPORTED', '"mcss all" is not a supported command')
+    process.exit()
   }
 
-  if (!args.source) {
-    const cwd = process.cwd()
-    args.source = cwd
-    log.info('--source not specified, using process.cwd()', cwd)
+  if (!args.in) {
+    args.in = cwd
+    log.info('--in not specified, using process.cwd()', args.in)
+  } else if (!path.isAbsolute(args.in)) {
+    args.in = path.join(cwd, args.in)
   }
 
-  const files = await fs.getFiles(args.source)
-  console.log({ files })
+  if (!args.in.endsWith('index.js')) {
+    args.in = path.join(args.in, 'index.js')
+  }
+
+  const { default: theme } = await import(args.in)
+
+  const result = await css(theme)
+
+  if (commands.stringify) {
+    const style = args.hasOwnProperty('minified') ? result.minified : result.css
+    maybeWrite({ args, commands, style })
+  } else if (commands.parse) {
+    const style = JSON.stringify(result.parsed, null, 2)
+    maybeWrite({ args, commands, style })
+  } else if (commands.full) {
+    const style = JSON.stringify(result, null, 2)
+    maybeWrite({ args, commands, style })
+  } else {
+    log.error('MISSING_COMMAND', 'Either parse, stringify or full are required.')
+  }
 }
 
 run()
